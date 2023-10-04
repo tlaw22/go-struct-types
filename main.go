20d66acd51bbf3b3be4c17349625f3d5e363e66b
Tim Lawless <108081811+tlaw22@users.noreply.github.com>
2023-10-04 15:01:52 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+type User struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	BirthDate   time.Time
+}
+
+func main() {
+	user := User{
+		FirstName:   "Trevor",
+		LastName:    "Sawler",
+		PhoneNumber: "1-555-111-2222",
+	}
+
+	log.Println(user.FirstName, user.LastName, user.PhoneNumber)
+
+}
